72345e15f9314d4349fb4cc05cee5d358e91808c
Charlotte.Hong <hunandy14@gmail.com>
2022-02-27 13:45:51 +0900

    電腦健康狀態檢查

--- README.md
+++ README.md
@@ -8,5 +8,9 @@
 irm bit.ly/3Jcl0h1|iex; Win11Update-NotVerify -IsoFile:"D:\Win11_Chinese(Traditional)_x64v1.iso"
 ```
 
-### 電腦健康狀態檢查
-WindowsPCHealthCheckSetup：https://www.microsoft.com/en-us/software-download/windowsinsiderpreviewpchealth
+### 電腦健康狀態檢查 (WindowsPCHealthCheckSetup)
+官方載點：https://www.microsoft.com/en-us/software-download/windowsinsiderpreviewpchealth
+
+```
+irm bit.ly/3Jcl0h1|iex; Install-WindowsPCHealthCheckSetup
+```
--- Win11Update-NotVerify.ps1
+++ Win11Update-NotVerify.ps1
@@ -23,3 +23,17 @@ function Win11Update-NotVerify {
     Write-Host "安裝時務必在第一個畫面選擇" -ForegroundColor:Yellow
     Write-Host "  變更安裝下載的方式 -> 現在不用" -ForegroundColor:Yellow
 } # Win11Update-NotVerify -IsoFile:"D:\DATA\ISO_Files\Win11_Chinese(Traditional)_x64v1.iso"
+
+
+function Install-WindowsPCHealthCheckSetup {
+    param (
+        [switch] $Force
+    )
+    $Site = "https://github.com/hunandy14/Win11Update-NotVerify/raw/master/soft/WindowsPCHealthCheckSetup.msi"
+    $FileName    = "WindowsPCHealthCheckSetup.msi"
+    $AppPath     = $env:TEMP
+    $DownloadChk = !(Test-Path "$AppPath\$FileName")
+    if ($DownloadChk -or $Force) {
+        Start-BitsTransfer $Site "$env:TEMP"
+    } explorer "$AppPath\$FileName"
+} # Install-WindowsPCHealthCheckSetup
