688546c1b9b60aeff9296e542e8275c0d3feb2b5
Charlotte.Hong <hunandy14@gmail.com>
2025-01-25 22:50:28 +0900

    抽出 7z 確認的邏輯

--- Win11Update-NotVerify.ps1
+++ Win11Update-NotVerify.ps1
@@ -1,17 +1,24 @@
-# 跳過 Windows11 更新的硬體限制
-function Win11Update-NotVerify {
+function Initialize-7zEnvironment {
     param (
-        [string] $IsoFile
+        [switch] $ForceInstall
     )
     $7zPATH = "${env:ProgramFiles}\7-Zip"
     
-    if (!(Test-Path $7zPATH)) {
+    if ((!(Test-Path $7zPATH)) -or $ForceInstall) {
         Set-ExecutionPolicy Bypass -S:Process -F
         Invoke-RestMethod chocolatey.org/install.ps1|Invoke-Expression
         choco install -y 7zip
     }
     
     $env:Path = "${env:Path};$7zPATH"
+} # Initialize-7zEnvironment
+
+# 跳過 Windows11 更新的硬體限制
+function Update-Win11 {
+    param (
+        [string] $IsoFile
+    )
+    Initialize-7zEnvironment -ForceInstall
     $WinPATH = "${env:Temp}\Win11_ISO"
     $cmd = "7z x `"$IsoFile`" -o$WinPATH"
     Invoke-Expression $cmd
@@ -22,7 +29,7 @@ function Win11Update-NotVerify {
     
     Write-Host "安裝時務必在第一個畫面選擇" -ForegroundColor:Yellow
     Write-Host "  變更安裝下載的方式 -> 現在不用" -ForegroundColor:Yellow
-} # Win11Update-NotVerify -IsoFile:"D:\DATA\ISO_Files\Win11_Chinese(Traditional)_x64v1.iso"
+} # Update-Win11 -IsoFile:"D:\DATA\ISO_Files\Win11_Chinese(Traditional)_x64v1.iso"
 
 
 function Install-WindowsPCHealthCheckSetup {
